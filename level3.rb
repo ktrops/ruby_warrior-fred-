da49e58dc8288f76d2ff83b2aff6e74f03939d13
Katie Atrops <katrops@gmail.com>
2015-06-09 10:18:24 -0700

    added level3

--- level3.rb
+++ level3.rb
@@ -1,6 +1,8 @@
 class Player
   def play_turn(warrior)
-    if warrior.feel.empty?
+    if warrior.health < 20 && warrior.feel.empty?
+      warrior.rest!
+    elsif warrior.feel.empty?
       warrior.walk!
     else
       warrior.attack!
